b434baa6d84ae930d428d24c07ee1613fcdf42f1
jackie <jackie123768@gmail.com>
2025-07-04 03:48:52 -0700

    add dropdown text

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 import TextBlock from './components/textBlock'
@@ -12,9 +10,9 @@ function App() {
       <h1 className="title">How to Make Tea</h1>
       <TextBlock title="Intro" text="Today you'll learn you how to make a hot cup of tea. Take this time to relax and breathe. Forget about everything going on around you, all that matters is that you are here in this moment."></TextBlock>
       <TextBlock title="Preparing your water" text="First let's prepare the hot water. Grab a kettle and fill it with water to your desired amount."></TextBlock>
-      <TextBlock title="Boil the water" text="Press the start button located on your kettle to start boiling the water"></TextBlock>
-      <TextBlock title="Prepare the tea leaves" text="Measure out the desired amount of dry tea leaves. It is generally recommended to use one teaspoon of tea leaves for every 6 ounces of water. This equates to one cup of tea"></TextBlock>
-      <TextBlock title="Pour Hot Water" text="Once your water has finished boiling. Carefully pour your hot water into the teapot”"></TextBlock>
+      <TextBlock title="Boiling the water" text="Press the start button located on your kettle to start boiling the water"></TextBlock>
+      <TextBlock title="Preparing the tea leaves" text="Measure out the desired amount of dry tea leaves. It is generally recommended to use one teaspoon of tea leaves for every 6 ounces of water. This equates to one cup of tea"></TextBlock>
+      <TextBlock title="Pour hot water" text="Once your water has finished boiling. Carefully pour your hot water into the teapot”"></TextBlock>
       <TextBlock title="Steeping" text="Now sit back and let the tea steep. Each tea has a different steeping point but if you you're feeling too lazy, just set a timer for 4 minutes"></TextBlock>
       <TextBlock title="Enjoy your tea" text="Carefully pour a cup of tea for yourself. Take your tea back to your desk or take the time to enjoy your cup of tea in this moment"></TextBlock>
     </div>
--- src/components/textBlock.tsx
+++ src/components/textBlock.tsx
@@ -1,10 +1,25 @@
 import "/Users/jacki/Desktop/School/Classes/Summer25/Art23/exp20/Tea/src/components/textBlock.css"
+import {useState} from 'react'
 
 export default function TextBlock(props) {
+    const [clicked, setClicked] = useState(false);
+
+    function dropDown() {
+        setClicked(!clicked);
+    }
+
     return (
         <div className="container">
-            <h2 className="blackText">{props.title}</h2>
-            <p className="blackText">{props.text}</p>
+            
+            <div className="headerContainer">
+                <h2 className="blackText">{props.title}</h2>
+                {clicked ? 
+                (<img onClick={() => dropDown()} className="caret" src="/caret-down.png"></img>) :
+                (<img onClick={() => dropDown()} className="caret" src="/caret-right.png"></img>
+                )}
+            </div>
+            
+            {clicked && <p className="blackText">{props.text}</p>}
         </div>
     )
 }
